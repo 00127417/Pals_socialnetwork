03284b24f8d1e29f39b5a946a569b6d7ccdc9cd9
00127417 <00127417@uca.edu.sv>
2018-11-30 11:18:32 -0600

    se agrego el modelo imagenes

---
+++ models/imagenes.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+
+var img_schema = new schema({
+    post:{type: String},
+    title:{type: String},
+    path: {type: String,default: ""},
+    owner: {type: String}
+});
+
+var Imagen = mongoose.model("Imagen",img_schema);
+
+module.exports = Imagen;
