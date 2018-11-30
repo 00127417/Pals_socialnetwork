b68930b5995b45c1a13ec6d4a0352e8bfad290f8
00127417 <00127417@uca.edu.sv>
2018-11-30 11:27:52 -0600

    Se modificaron los archivos de la carpeta public

--- public/script/main.js
+++ public/script/main.js
@@ -1,58 +1,68 @@
 let app = {
-    init: function () {
-        this.addEvents();
-    },
-    addEvents: function () {
-        let loadContent = function () {
-            fetch("/post")
-                .then(res => res.json())
-                .then(data => {
-                    let posts = document.getElementsByClassName("posts")[0];
-                    posts.innerHTML = data.reduce((cadena, element) => {
-                        return cadena +
-                            ` 
-
-                            <div class="col s12 m8 offset-m2 l6 offset-l3">
-                            <div class="card-panel grey lighten-5 z-depth-1">
-                              <div class="row valign-wrapper">
-                                <div class="col s2">
-                                  <img src="/img/profileholder.png " alt="" class="circle responsive-img smallpic"> <!-- notice the "circle" class -->
-                                </div>
-                                <div class="col s12">
-                                  <h4 class="teal-text ">
-                                  ${element.owner}
-                                  </h4>
-                                  <span class="black-text">
-                                    ${element.post}
-                                  </span>
-                                </div>
+    
+  init: function () {
+      this.addEvents();
+  },
+  addEvents: function () {
+      let loadContent = function () {
+          
+          fetch("/img")
+              .then(res => res.json())
+              .then(data => {
+                  let posts = document.getElementsByClassName("posts")[0];
+                  posts.innerHTML = data.reduce((cadena, element) => {
+                      if(element.title=="" && element.title){
+                        var cadenaux="";
+                      }else{
+                        var cadenaux=`<img class="materialboxed responsive-img" width="700" src="../uploads/${element.path}"></img>`;
+                      }
+                      return cadena +
+                      ` 
+                      <div class="section">
+                        <div class="col s12 m8 offset-m2 l6 offset-l3">
+                          <div class="card-panel grey lighten-5 z-depth-1">
+                            <div class="row valign-wrapper">
+                              <div class="col s2">
+                                <img src="/img/profileholder.png " alt="" class="circle responsive-img smallpic"> <!-- notice the "circle" class -->
+                              </div>
+                              <div class="col s12">
+                                <h4 class="teal-text ">
+                                ${element.owner}
+                                </h4>
+                                <p class="black-text">
+                                  ${element.post}
+                                </p>
+                                <br>
+                                ${cadenaux}
                               </div>
                             </div>
                           </div>
-                          
+                        </div>
+                      </div>
 
-                            `
-                    }, "");
-        
-                });
-        }
-        let form = document.forms.savePost;
+                      `
+                  }, "");
+              });
+      }
+      let form = document.forms.savePost;
 
-        form.addEventListener('submit', function (event) {
-            event.preventDefault();
-            fetch(form.action, {
-                    method: 'POST',
-                    body: new URLSearchParams(new FormData(form))
-                }).then(res => res.json())
-                .then(data => {
-                    console.log(data);
-                    form.post.value="";
-                    loadContent();
-                });
-        });
+      form.addEventListener('submit', function (event) {
+          event.preventDefault();
+          fetch(form.action, {
+                  method: 'POST',
+                  body: new FormData(form)
+              }).then(res => res.json())
+              .then(data => {
+                  //console.log(data);
+                  form.post.value="";
+                  form.imagen.value = "";
+                  loadContent();
+              });
 
-        loadContent();
+      });
 
-    }
+      loadContent();
+
+  }
 };
 window.onload = () => app.init();
